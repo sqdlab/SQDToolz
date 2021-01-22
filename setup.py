1d699ed113df24774e9730a940d4ae673ec677e8
27rohitb <14235283+27rohitb@users.noreply.github.com>
2021-01-22 12:01:11 +1000

    Fixed setup file to include HAL and drivers

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="sqdtoolz",
-    version="0.0.1",
-    author="Prasanna Paikkam, Rohit Beriwal",
-    author_email="p.paikkam@uq.edu.au, r.beriwal@uq.edu.au",
+    version="0.0.1.dev1",
+    author="Prasanna Pakkiam, Rohit Beriwal",
+    author_email="p.pakkiam@uq.edu.au, r.beriwal@uq.edu.au",
     description="toolbox to control and run a experiments",
     url="https://github.com/sqdlab/sqdtoolz",
     packages=setuptools.find_packages(),
@@ -16,7 +16,10 @@ setuptools.setup(
         "Programming Language :: Python :: 3 :: ONLY",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     keywords='control toolbox, timing',
-    install_requires=['numpy','qcodes','matplotlib','scipy']
+    install_requires=['numpy','qcodes>=0.21.0','matplotlib','scipy','pyvisa-py'],
+    package_data={
+        "sqdtoolz":["Drivers/*.py","HAL/*.py"]
+    }
 )

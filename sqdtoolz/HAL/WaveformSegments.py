5483d46199a80572ceac88439d6d5aff8154cb64
Prasanna Pakkiam <p.pakkiam@uq.edu.au>
2021-02-23 08:48:25 +1000

    Added a WFM_Group class to group wavefom segments to enable elastic time-segments.

--- sqdtoolz/HAL/WaveformSegments.py
+++ sqdtoolz/HAL/WaveformSegments.py
@@ -12,14 +12,27 @@ class WaveformSegmentBase:
     def NumPts(self, fs):
         return self.Duration*fs
 
-    def get_waveform(self, fs, t0, ch_index):
+    def get_waveform(self, fs, t0_ind, ch_index):
+        '''
+        Returns the final waveform; that is, after applying the modification function.
+
+        Inputs:
+            - fs - Sample Rate in Hertz
+            - t0_ind - Initial time (useful when the actual time in the waveform is important like with the phase of sinusoidal
+                   modulation or perhaps a continual ramp/waveform that is to continue rather than resetting back to zero).
+                   Note that t0 is given as the point index (0 for t=0 being the first point of the entire waveform).
+            - ch_index - Dimension/index of the waveform; useful when the modification function is a function of dimnension
+                         in ND waveforms.
+        
+        Returns a numpy array of points representing the total waveform.
+        '''
         cur_wfm = self._get_waveform(fs, t0, ch_index)
         if self._mod_func:
             return self._mod_func.modify_waveform(cur_wfm, fs, t0, ch_index)
         else:
             return cur_wfm
 
-    def _get_waveform(self, fs, t0, ch_index):
+    def _get_waveform(self, fs, t0_ind, ch_index):
         assert False, "Waveform Segment classes must implement a get_waveform function."
 
     def _get_current_config(self):
@@ -43,6 +56,60 @@ class WaveformSegmentBase:
         for cur_key in ["Name", "Mod Func"]:
             assert cur_key in config_dict, "Configuration dictionary does not have the key: " + cur_key
         
+class WFS_Group(WaveformSegmentBase):
+    def __init__(self, name, wfm_segs, time_len=-1, mod_func=None):
+        super().__init__(name, mod_func)
+        self._abs_time = time_len   #_abs_time is the total absolute time (if -1, the duration is the sum of the individual time segment durations)
+        self._wfm_segs = wfm_segs
+        self._validate_wfm_segs()
+
+    @property
+    def Duration(self):
+        if self._abs_time == -1:
+            return sum([x.Duration for x in self._wfm_segs])
+        else:
+            return self._abs_time
+    @Duration.setter
+    def Duration(self, len_seconds):
+        self._abs_time = len_seconds
+    
+    def _validate_wfm_segs(self):
+        elastic_segs = []
+        for ind_wfm, cur_wfm_seg in enumerate(self._wfm_segs):
+            if cur_wfm_seg.Duration == -1:
+                elastic_segs += [ind_wfm]
+        assert len(elastic_segs) <= 1, "There are too many elastic waveform segments (cannot be above 1)."
+        if self._abs_time == -1:
+            assert len(elastic_segs) == 0, "If the total waveform length is unbound, the number of elastic segments must be zero."
+        if self._abs_time > 0 and len(elastic_segs) == 0:
+            assert sum([x.Duration for x in self._wfm_segs]) == self._abs_time, "Sum of waveform segment durations do not match the total specified waveform group time. Consider making one of the segments elastic by setting its duration to be -1."
+        
+        #Return the elastic segment index
+        if len(elastic_segs) > 0:
+            return -1
+        else:
+            return elastic_segs[0]
+
+    def _get_waveform(self, fs, t0_ind, ch_index):
+        elas_seg_ind = self._validate_wfm_segs()
+        
+        #Calculate and set the elastic time segment
+        if elas_seg_ind != -1:  #Note that self._abs_time > 0
+            self._wfm_segs[elas_seg_ind].Duration = self._abs_time - (sum([x.Duration for x in self._wfm_segs])+1)    #Negate the -1 segment
+
+        #Concatenate the individual waveform segments
+        final_wfm = np.array([])
+        t0_ind = 0
+        for cur_wfm_seg in self._wfm_segment_list:
+            #TODO: Preallocate - this is a bit inefficient...
+            final_wfm = np.concatenate((final_wfms, cur_wfm_seg.get_waveform(self._sample_rate, t0_ind, ch_index)))
+            t0_ind += final_wfm.size
+
+        #Reset segment to be elastic
+        if elas_seg_ind != -1:
+            self._wfm_segs[elas_seg_ind].Duration = -1
+        
+        return final_wfm
 
 class WFS_Constant(WaveformSegmentBase):
     def __init__(self, name, mod_func, time_len, value=0.0):
@@ -66,7 +133,7 @@ class WFS_Constant(WaveformSegmentBase):
     def Duration(self, len_seconds):
         self._duration = len_seconds
 
-    def _get_waveform(self, fs, t0, ch_index):
+    def _get_waveform(self, fs, t0_ind, ch_index):
         return np.zeros(round(self.NumPts(fs))) + self._value
 
     def _get_current_config(self):
@@ -104,7 +171,7 @@ class WFS_Gaussian(WaveformSegmentBase):
     def _gauss(x):
         return np.exp(-x*x / (2*self._sigma*self._sigma))
 
-    def _get_waveform(self, fs, t0, ch_index):
+    def _get_waveform(self, fs, t0_ind, ch_index):
         n = self.NumPts(fs)
         #Generate the sample points on the Gaussian (start and end points are the same)
         sample_points = np.linspace(-self._num_sd, self._num_sd, int(np.round(n)))

6c849b802d1d2e877a8fa7dc7b4a3b0e7a986bb3
Prasanna Pakkiam <p.pakkiam@uq.edu.au>
2021-11-24 14:49:18 +1000

    Made Lorentzian fit pick smarter initial conditions.

--- sqdtoolz/Utilities/DataFitting.py
+++ sqdtoolz/Utilities/DataFitting.py
@@ -6,7 +6,7 @@ class DFitPeakLorentzian:
     def __init__(self):
         pass
 
-    def get_fitted_plot(self, data_x, data_y, xLabel="", dip = False):
+    def get_fitted_plot(self, data_x, data_y, xLabel="", dip = False, dontplot=False):
         def func(x, a, w, x0, c):
             return a * (0.5*w)**2/((x-x0)**2 + (0.5*w)**2) + c
 
@@ -18,11 +18,12 @@ class DFitPeakLorentzian:
         #Calculate initial guesses...
         c0 = 0.5 * ( np.mean(data_y[:int(data_y.size*0.1)]) + np.mean(data_y[-int(data_y.size*0.1):]) )
         w0 = 0.2 * (xMax - xMin)
-        x00 = np.mean(data_x)
         if dip:
             a0 = np.min(data_y) - c0
+            x00 = data_x[np.argmin(data_y)]
         else:
             a0 = np.max(data_y) - c0
+            x00 = data_x[np.argmax(data_y)]
 
         if dip:
             popt, pcov = scipy.optimize.curve_fit(func, data_x, data_y, [a0, w0, x00, c0],
@@ -31,11 +32,13 @@ class DFitPeakLorentzian:
             popt, pcov = scipy.optimize.curve_fit(func, data_x, data_y, [a0, w0, x00, c0],
                                              bounds=([0, np.abs(data_x[1]-data_x[0]), xMin, -np.inf], [1.2*a0, 0.9*(xMax - xMin), xMax, np.inf]))
 
-        fig, axs = plt.subplots(1)
-        axs.plot(data_x, data_y, 'kx')
-        axs.plot(data_x, func(data_x, *popt), 'r-')
-        axs.set_xlabel(xLabel)
-        axs.set_ylabel('IQ Amplitude')
+        fig = None
+        if not dontplot:
+            fig, axs = plt.subplots(1)
+            axs.plot(data_x, data_y, 'kx')
+            axs.plot(data_x, func(data_x, *popt), 'r-')
+            axs.set_xlabel(xLabel)
+            axs.set_ylabel('IQ Amplitude')
 
         datapkt = {
             'amplitude' : popt[0],

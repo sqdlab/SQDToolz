Fixed matplotlib-induced memory leak and separated IQ normalisation into its own utility.

sqdtoolz/Experiments/Experimental/ExpT1GE.py
@@ -17,6 +17,8 @@ class ExpT1GE(Experiment):
 
         self._SPEC_qubit = SPEC_qubit
 
+        self._dont_show_plot = kwargs.get('dont_show_plot', False)
+
         #Calculate default load-time via T1 of qubit or default to 40e-6
         def_load_time = self._SPEC_qubit['GE T1'].Value * 4
         if def_load_time == 0:
@@ -88,7 +90,7 @@ class ExpT1GE(Experiment):
         dfit = DFitExponential()
         if self.normalise_data:
             data_raw_IQ = np.vstack([ arr[:,self._iq_indices[0]], arr[:,self._iq_indices[1]] ]).T
-            fig, axs = plt.subplots(ncols=2, gridspec_kw={'width_ratios':[2,1]})
+            fig, axs = plt.subplots(ncols=2, gridspec_kw={'width_ratios':[2,1]}, clear=True, num=1)
             fig.set_figheight(5); fig.set_figwidth(15)
             data_y = self.norm_expt.normalise_data(data_raw_IQ, ax=axs[1])
 
@@ -107,9 +109,11 @@ class ExpT1GE(Experiment):
 
         print(dpkt)
         if self.normalise_data:
-            fig.show()
+            if not self._dont_show_plot:
+                fig.show()
             fig.savefig(self._file_path + 'fitted_plot.png')
         else:
-            dpkt['fig'].show()
+            if not self._dont_show_plot:
+                dpkt['fig'].show()
             dpkt['fig'].savefig(self._file_path + 'fitted_plot.png')
               

sqdtoolz/Utilities/DataFitting.py
@@ -109,7 +109,7 @@ class DFitExponential:
     def __init__(self):
         pass
 
-    def get_fitted_plot(self, data_x, data_y, xLabel="", rise = False, fig=None, axs=None):
+    def get_fitted_plot(self, data_x, data_y, xLabel="", rise = False, fig=None, axs=None, dontplot=False):
         def func(x, a, c, tau):
             return a * np.exp(-x/tau) + c
 
@@ -137,12 +137,15 @@ class DFitExponential:
             popt, pcov = scipy.optimize.curve_fit(func, data_x, data_y, [a0, c0, tau0],
                                              bounds=([0, yMin, 0.01*tau0], [2.0*a0, yMax, tau0*10]))
 
-        if fig == None:
-            fig, axs = plt.subplots(1)
-        axs.plot(data_x, data_y, 'kx')
-        axs.plot(data_x, func(data_x, *popt), 'r-')
-        axs.set_xlabel(xLabel)
-        axs.set_ylabel('IQ Amplitude')
+        if not dontplot:
+            if fig == None:
+                fig, axs = plt.subplots(1)
+            axs.plot(data_x, data_y, 'kx')
+            axs.plot(data_x, func(data_x, *popt), 'r-')
+            axs.set_xlabel(xLabel)
+            axs.set_ylabel('IQ Amplitude')
+        else:
+            fig = None
 
         datapkt = {
             'amplitude' : np.abs(popt[0]),
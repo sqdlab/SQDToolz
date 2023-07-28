7a5436019a33e33942b28c1d197c155f163debfc
Prasanna Pakkiam <p.pakkiam@uq.edu.au>
2023-07-28 10:39:13 +1000

    Added a Keysight P9373A driver.

---
+++ sqdtoolz/Drivers/VNA_Keysight_P9373A.py
@@ -0,0 +1,14 @@
+from sqdtoolz.Drivers.VNA_Agilent_N5232A import VNA_Agilent_N5232A
+
+class VNA_Keysight_P9373A(VNA_Agilent_N5232A):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.parameters.pop('elec_delay_time')
+
+    @property
+    def ElecDelayTime(self):
+        return -1
+    @ElecDelayTime.setter
+    def ElecDelayTime(self, val):
+        pass

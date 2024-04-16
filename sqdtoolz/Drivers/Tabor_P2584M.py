Fixed averaging issues on Tabor.

sqdtoolz/Drivers/Tabor_P2584M.py
@@ -746,6 +746,7 @@ class TaborP2584M_ACQ(InstrumentChannel):
         self._last_dsp_state = {}
         self._last_dsp_order = None
         self._last_dec_state = []
+        self._last_dsp_avg_reps = 0
 
         self._dsp_kernel_coefs = [None]*10
 
@@ -1241,6 +1242,8 @@ class TaborP2584M_ACQ(InstrumentChannel):
 
         if isinstance(cur_processor, ProcessorFPGA) and len(cur_processor.pipeline) == 0:
             return final_dsp_order
+        elif not isinstance(cur_processor, ProcessorFPGA):
+            return final_dsp_order
         else:
             final_dsp_order['dsp_active'] = True
 
@@ -1459,13 +1462,13 @@ class TaborP2584M_ACQ(InstrumentChannel):
 
         if final_dsp_order['avRepetitions']:
             wavlen = int(num_bytes // 4)    #32bits as it's a 28bit counter...
-            fin_shape = (1, self.NumSegments, int(self.NumSamples*12/10))
+            fin_shape = (1, int(self.NumSamples*12/10)) #self.NumSegments, 
         else:
             wavlen = int(num_bytes // 2)
             if final_dsp_order['dsp_active']:
-                fin_shape = (self.NumRepetitions, self.NumSegments, int(self.NumSamples*12/10))
+                fin_shape = (self.NumRepetitions, int(self.NumSamples*12/10)) #self.NumSegments, 
             else:
-                fin_shape = (self.NumRepetitions, self.NumSegments, self.NumSamples)
+                fin_shape = (self.NumRepetitions, self.NumSamples) #self.NumSegments, 
         # Ensure all previous commands have been executed
         while (not self._parent._get_cmd('*OPC?')):
             pass
@@ -1501,39 +1504,43 @@ class TaborP2584M_ACQ(InstrumentChannel):
                     'misc' : {}
                 }
         if final_dsp_order['avRepetitions']:
-            ret_val['parameters'] = ['sample']
+            if final_dsp_order['avSamples']:
+                ret_val['parameters'] = []
+            else:
+                ret_val['parameters'] = ['sample']
         else:
             ret_val['parameters'] = ['repetition', 'sample']
-        #TODO: Change after new firmware release...
-        ret_val['parameters'].pop(-1)        
+            #TODO: Change after new firmware release...
+            ret_val['parameters'].pop(-1)
         leSampleRates = []
-        num_ch_divs = final_dsp_order['num_ch_divs']
         final_scale = final_dsp_order['final_scale_factor']
         if not final_dsp_order['dsp_active']:
             # Only return data which matches channels that are active
             if (self.ChannelStates[0]) :
-                ret_val['data']['CH1'] = self.conv_data(wav1) * final_scale
+                ret_val['data']['CH1'] = self.conv_data(wav1, final_dsp_order) * final_scale
                 leSampleRates += [self.SampleRate]
             if (self.ChannelStates[1]) :
-                ret_val['data']['CH2'] = self.conv_data(wav2) * final_scale
+                ret_val['data']['CH2'] = self.conv_data(wav2, final_dsp_order) * final_scale
                 leSampleRates += [self.SampleRate]
+            ret_val['parameters'] = ['repetition', 'segment', 'sample']
             ret_val['misc']['SampleRates'] = leSampleRates
         else:
+            num_ch_divs = final_dsp_order['num_ch_divs']
             if num_ch_divs[0] > 0:
                 wav1 = self.conv_data(wav1, final_dsp_order)
             if final_dsp_order['doFFT']:
-                ret_val['data']['fft_real'] = wav1[:,:,int(2*5+1)::12] * final_scale
-                ret_val['data']['fft_imag'] = wav1[:,:,int(2*5)::12] * final_scale
+                ret_val['data']['fft_real'] = wav1[:,int(2*5+1)::12] * final_scale
+                ret_val['data']['fft_imag'] = wav1[:,int(2*5)::12] * final_scale
                 #
                 wav2 = self.conv_data(wav2, final_dsp_order)
-                ret_val['data']['debug_time_I'] = wav2[:,:,int(2*5+1)::12] * final_scale
-                ret_val['data']['debug_time_Q'] = wav2[:,:,int(2*5)::12] * final_scale
+                ret_val['data']['debug_time_I'] = wav2[:,int(2*5+1)::12] * final_scale
+                ret_val['data']['debug_time_Q'] = wav2[:,int(2*5)::12] * final_scale
             else:
                 offset = 0
                 for m in range(num_ch_divs[0]):
                     if final_dsp_order['avRepetitions']:
-                        ret_val['data'][f'CH1_{m}_I'] = wav1[0,:,int(2*(m+offset)+1)::2][:,:int(self.NumSamples/10)] * final_scale*self.NumRepetitions  #TODO: Review after new firmware release
-                        ret_val['data'][f'CH1_{m}_Q'] = wav1[0,:,int(2*(m+offset))::2][:,:int(self.NumSamples/10)] * final_scale*self.NumRepetitions    #TODO: Review after new firmware release
+                        ret_val['data'][f'CH1_{m}_I'] = wav1[0,int(2*(m+offset)+1)::2][:int(self.NumSamples/10)] * final_scale*self.NumRepetitions  #TODO: Review after new firmware release
+                        ret_val['data'][f'CH1_{m}_Q'] = wav1[0,int(2*(m+offset))::2][:int(self.NumSamples/10)] * final_scale*self.NumRepetitions    #TODO: Review after new firmware release
                         #TODO: Change after new firmware release...
                         if final_dsp_order['avSamples']:
                             ret_val['data'][f'CH1_{m}_I'] = np.array([np.sum(ret_val['data'][f'CH1_{m}_I'])])*2.0
@@ -1542,8 +1549,8 @@ class TaborP2584M_ACQ(InstrumentChannel):
                         wav1 =self.conv_data(wav2, final_dsp_order)
                         offset = -1
                     else:
-                        ret_val['data'][f'CH1_{m}_I'] = wav1[:,:,int(2*(m+offset)+1)::12] * final_scale
-                        ret_val['data'][f'CH1_{m}_Q'] = wav1[:,:,int(2*(m+offset))::12] * final_scale
+                        ret_val['data'][f'CH1_{m}_I'] = wav1[:,int(2*(m+offset)+1)::12] * final_scale
+                        ret_val['data'][f'CH1_{m}_Q'] = wav1[:,int(2*(m+offset))::12] * final_scale
                     leSampleRates += [self.SampleRate / 10]*2
                     if m == 4:
                         wav1 = self.conv_data(wav2, final_dsp_order)    #This is relevant in the BIND mode...
@@ -1552,15 +1559,15 @@ class TaborP2584M_ACQ(InstrumentChannel):
                     wav2 =self.conv_data(wav2, final_dsp_order)
                 for m in range(num_ch_divs[1]):
                     if final_dsp_order['avRepetitions']:
-                        ret_val['data'][f'CH2_{m}_I'] = wav2[0,:,int(2*m+1)::10] * final_scale*self.NumRepetitions
-                        ret_val['data'][f'CH2_{m}_Q'] = wav2[0,:,int(2*m)::10] * final_scale*self.NumRepetitions
+                        ret_val['data'][f'CH2_{m}_I'] = wav2[0,int(2*m+1)::12] * final_scale*self.NumRepetitions
+                        ret_val['data'][f'CH2_{m}_Q'] = wav2[0,int(2*m)::12] * final_scale*self.NumRepetitions
                         #TODO: Change after new firmware release...
                         if final_dsp_order['avSamples']:
                             ret_val['data'][f'CH2_{m}_I'] = np.array([np.sum(ret_val['data'][f'CH2_{m}_I'])])*2.0
                             ret_val['data'][f'CH2_{m}_Q'] = np.array([np.sum(ret_val['data'][f'CH2_{m}_Q'])])*2.0
                     else:
-                        ret_val['data'][f'CH2_{m}_I'] = wav2[:,:,int(2*m+1)::12] * final_scale
-                        ret_val['data'][f'CH2_{m}_Q'] = wav2[:,:,int(2*m)::12] * final_scale
+                        ret_val['data'][f'CH2_{m}_I'] = wav2[:,int(2*m+1)::12] * final_scale
+                        ret_val['data'][f'CH2_{m}_Q'] = wav2[:,int(2*m)::12] * final_scale
                     leSampleRates += [self.SampleRate / 10]*2
                 ret_val['misc']['SampleRates'] = leSampleRates
         return ret_val
@@ -1605,7 +1612,7 @@ class TaborP2584M_ACQ(InstrumentChannel):
             assert self.NumSamples % 48 == 0, "The number of samples must be divisible by 48 if in DUAL mode."
             final_dsp_order = self.settle_dsp_processors(cur_processor)
         else:
-            if not cur_processor.compare_pipeline_state(self._last_dsp_state):
+            if not cur_processor.compare_pipeline_state(self._last_dsp_state) or (self._last_mem_frames_segs_samples_avg[3] and self._last_dsp_avg_reps != self.NumRepetitions):
                 final_dsp_order = self.settle_dsp_processors(cur_processor)
                 reprogram_dsps = True
             else:
@@ -1657,6 +1664,7 @@ class TaborP2584M_ACQ(InstrumentChannel):
                 self.averageEnable(False)
 
             self._last_dsp_state = cur_processor.get_pipeline_state()
+            self._last_dsp_avg_reps = self.NumRepetitions
             self._last_dsp_order = final_dsp_order
 
         decision_blocks = kwargs.get('decision_blocks', [])

tests/AWG_Tabor_Test_ADCDSP.py
@@ -51,6 +51,9 @@ pt_centre = np.array([0,0])
 num_corners = 12
 poly_size = 0.05
 
+# Setup waveforms
+awg_wfm = WaveformAWG("Waveform", lab,  [(['TaborAWG', 'AWG'], 'CH1'), (['TaborAWG', 'AWG'], 'CH2')], 2.0e9, 40e-6)
+
 def add_segments_circle(awg_wfm, pt_centre, num_corners, poly_size):
     awg_wfm.clear_segments()
     awg_wfm.add_waveform_segment(WFS_Constant(f"init", lab.WFMT("OscMod").apply(phase=0), 96e-9, 0.25))
@@ -66,221 +69,233 @@ def add_segments_circle(awg_wfm, pt_centre, num_corners, poly_size):
     # Setup trigger 
     awg_wfm.get_output_channel(0).marker(2).set_markers_to_segments(read_segs)
 
-# Setup waveforms
-awg_wfm = WaveformAWG("Waveform", lab,  [(['TaborAWG', 'AWG'], 'CH1'), (['TaborAWG', 'AWG'], 'CH2')], 2.0e9, 40e-6)
 
 acq_module = ACQ("TaborACQ", lab, ['TaborAWG', 'ACQ'])
-acq_module.NumSamples = 5040    #(72*70)
 acq_module.NumSegments = 1
-acq_module.NumRepetitions = int(num_corners*3)
 acq_module.SampleRate = 2.5e9
 acq_module.ChannelStates = (True, True)
 
-filt_coeffs = scipy.signal.firwin(51, 10e6, fs=lab._get_instrument('TaborAWG').ACQ.sample_rate())
-# acq_module._instr_acq.setup_data_path(ddc_mode = "REAL", ddr_store = "DIR")
-
 ProcessorFPGA('fpga_dsp', lab)
 
-acq_module.NumRepetitions = 30
-awg_wfm.clear_segments()
-awg_wfm.add_waveform_segment(WFS_Constant(f"init", lab.WFMT("OscMod").apply(phase=0), 2048e-9, 0.25))
-awg_wfm.add_waveform_segment(WFS_Constant(f"pad", None, -1, 0.0))
-# Setup trigger 
-awg_wfm.get_output_channel(0).marker(2).set_markers_to_segments(['init'])
-# Prepare waveforms and output them
-awg_wfm.prepare_initial()
-awg_wfm.prepare_final()
-# Set output channel to true
-awg_wfm.get_output_channel(0).Output = True
-awg_wfm.get_output_channel(1).Output = True
-#
-# lab.PROC('fpga_dsp').add_stage(FPGA_DDCFIR([[{'fLO':100e6, 'fc':10e6, 'Taps':40}, {'fLO':105e6, 'fc':10e6, 'Taps':40}]]))
-lab.PROC('fpga_dsp').reset_pipeline()
-lab.PROC('fpga_dsp').add_stage(FPGA_DDC([[105e6],[100e6]]))
-lab.PROC('fpga_dsp').add_stage(FPGA_Decimation('sample', 10))
-acq_module.set_data_processor(lab.PROC('fpga_dsp'))
-leData = acq_module.get_data()['data']
-#
-lab.PROC('fpga_dsp').reset_pipeline()
-lab.PROC('fpga_dsp').add_stage(FPGA_DDC([[105e6],[100e6]]))
-lab.PROC('fpga_dsp').add_stage(FPGA_Decimation('sample', 10))
-lab.PROC('fpga_dsp').add_stage(FPGA_Integrate('repetition'))
-#
-acq_module.set_data_processor(lab.PROC('fpga_dsp'))
-leData2 = acq_module.get_data()['data']
-#
-fig, ax = plt.subplots(nrows=2)
-for r in range(acq_module.NumRepetitions) :
-    for s in range(acq_module.NumSegments) :
-        times = np.arange(acq_module.NumSamples/10)/(acq_module.SampleRate/10) * 1e9
-        ax[0].plot(times, leData['data']['CH1_0_I'][r][s])
-        ax[1].plot(times, leData['data']['CH1_0_Q'][r][s])
-        # ax[0].plot(leData['data']['CH1_1_I'][r][s])
-        # ax[1].plot(leData['data']['CH1_1_Q'][r][s])
-for s in range(acq_module.NumSegments):
-    # times = np.arange(acq_module.NumSamples)/acq_module.SampleRate * 1e9
-    ax[0].plot(times, leData2['data']['CH1_0_I'][s]/acq_module.NumRepetitions)
-    ax[1].plot(times, leData2['data']['CH1_0_Q'][s]/acq_module.NumRepetitions)
 
-###########
-#FFT TESTS#
-###########
-lab.WFMT("OscMod").IQFrequency = 400e6
-acq_module.NumRepetitions = 3
-awg_wfm.clear_segments()
-awg_wfm.add_waveform_segment(WFS_Constant(f"init", lab.WFMT("OscMod").apply(phase=0), 2048e-9, 0.25))
-awg_wfm.add_waveform_segment(WFS_Constant(f"pad", None, -1, 0.0))
-# Setup trigger 
-awg_wfm.get_output_channel(0).marker(2).set_markers_to_segments(['init'])
-# Prepare waveforms and output them
-awg_wfm.prepare_initial()
-awg_wfm.prepare_final()
-# Set output channel to true
-awg_wfm.get_output_channel(0).Output = True
-awg_wfm.get_output_channel(1).Output = True
-#
-lab.PROC('fpga_dsp').reset_pipeline()
-lab.PROC('fpga_dsp').add_stage(FPGA_DDC([[330e6],[]]))
-lab.PROC('fpga_dsp').add_stage(FPGA_Decimation('sample', 10))
-lab.PROC('fpga_dsp').add_stage(FPGA_FFT())
-acq_module.set_data_processor(lab.PROC('fpga_dsp'))
-acq_module.NumSamples = 10080
-leData = acq_module.get_data()['data']
-#
-fig, ax = plt.subplots(nrows=2)
-for r in range(acq_module.NumRepetitions) :
-    for s in range(acq_module.NumSegments) :
-        freqs = np.linspace(0, acq_module.SampleRate/10, 1024,endpoint=False)[:int(acq_module.NumSamples/10)]
-        ax[0].plot(freqs, np.abs(leData['data']['fft_real'][r][s] + 1j*leData['data']['fft_imag'][r][s]))
-        ax[1].plot(leData['data']['debug_time_I'][r][s])
-        ax[1].plot(leData['data']['debug_time_Q'][r][s])
-#
-lab.WFMT("OscMod").IQFrequency = 100e6
 
-# plt.show()
 
-#################
-#AVERAGING TESTS#
-#################
-acq_module.NumSamples = 5040
-acq_module.NumRepetitions = int(num_corners*3)
-fig, ax = plt.subplots(1)
-for s in range(4):
-    pt_centre = 0.1*np.array([np.cos(2*np.pi/4*s), np.sin(2*np.pi/4*s)])
 
-    add_segments_circle(awg_wfm, pt_centre, num_corners, poly_size)
+
+
+def test_basic_time_traces(lab):
+    awg_wfm = lab.HAL('Waveform')
+    acq_module = lab.HAL('TaborACQ')
+
+    acq_module.NumSamples = 5040    #(72*70)
+    acq_module.NumRepetitions = int(num_corners*3)
+
+    filt_coeffs = scipy.signal.firwin(51, 10e6, fs=lab._get_instrument('TaborAWG').ACQ.sample_rate())
+    # acq_module._instr_acq.setup_data_path(ddc_mode = "REAL", ddr_store = "DIR")
+
+    acq_module.NumRepetitions = 30
+    awg_wfm.clear_segments()
+    awg_wfm.add_waveform_segment(WFS_Constant(f"init", lab.WFMT("OscMod").apply(phase=0), 2048e-9, 0.25))
+    awg_wfm.add_waveform_segment(WFS_Constant(f"pad", None, -1, 0.0))
+    # Setup trigger 
+    awg_wfm.get_output_channel(0).marker(2).set_markers_to_segments(['init'])
     # Prepare waveforms and output them
     awg_wfm.prepare_initial()
     awg_wfm.prepare_final()
     # Set output channel to true
     awg_wfm.get_output_channel(0).Output = True
     awg_wfm.get_output_channel(1).Output = True
-
+    #
+    # lab.PROC('fpga_dsp').add_stage(FPGA_DDCFIR([[{'fLO':100e6, 'fc':10e6, 'Taps':40}, {'fLO':105e6, 'fc':10e6, 'Taps':40}]]))
     lab.PROC('fpga_dsp').reset_pipeline()
-    lab.PROC('fpga_dsp').add_stage(FPGA_DDCFIR([[{'fLO':100e6, 'fc':10e6, 'Taps':40}, {'fLO':100e6, 'fc':10e6, 'Taps':40}]]))
+    lab.PROC('fpga_dsp').add_stage(FPGA_DDC([[105e6],[100e6]]))
     lab.PROC('fpga_dsp').add_stage(FPGA_Decimation('sample', 10))
-    lab.PROC('fpga_dsp').add_stage(FPGA_Integrate('sample'))
-
     acq_module.set_data_processor(lab.PROC('fpga_dsp'))
     leData = acq_module.get_data()['data']
+    #
+    lab.PROC('fpga_dsp').reset_pipeline()
+    lab.PROC('fpga_dsp').add_stage(FPGA_DDC([[105e6],[100e6]]))
+    lab.PROC('fpga_dsp').add_stage(FPGA_Decimation('sample', 10))
+    lab.PROC('fpga_dsp').add_stage(FPGA_Integrate('repetition'))
+    #
+    acq_module.set_data_processor(lab.PROC('fpga_dsp'))
+    leData2 = acq_module.get_data()['data']
+    #
+    fig, ax = plt.subplots(nrows=2)
+    for r in range(acq_module.NumRepetitions) :
+        times = np.arange(acq_module.NumSamples/10)/(acq_module.SampleRate/10) * 1e9
+        ax[0].plot(times, leData['data']['CH1_0_I'][r])
+        ax[1].plot(times, leData['data']['CH1_0_Q'][r])
+        # ax[0].plot(leData['data']['CH1_1_I'][r][s])
+        # ax[1].plot(leData['data']['CH1_1_Q'][r][s])
+    # times = np.arange(acq_module.NumSamples)/acq_module.SampleRate * 1e9
+    ax[0].plot(times, leData2['data']['CH1_0_I']/acq_module.NumRepetitions)
+    ax[1].plot(times, leData2['data']['CH1_0_Q']/acq_module.NumRepetitions)
 
-    state_mkrs = ["o", ",", "d", "^", "s", "p", "h", "P"]
-    headers = acq_module._instr_acq.get_header_data(1)
-    headers = [state_mkrs[x['state1']] for x in headers]
 
+def test_FFT(lab):
+    awg_wfm = lab.HAL('Waveform')
+    acq_module = lab.HAL('TaborACQ')
+
+    lab.WFMT("OscMod").IQFrequency = 400e6
+    acq_module.NumRepetitions = 3
+    awg_wfm.clear_segments()
+    awg_wfm.add_waveform_segment(WFS_Constant(f"init", lab.WFMT("OscMod").apply(phase=0), 2048e-9, 0.25))
+    awg_wfm.add_waveform_segment(WFS_Constant(f"pad", None, -1, 0.0))
+    # Setup trigger 
+    awg_wfm.get_output_channel(0).marker(2).set_markers_to_segments(['init'])
+    # Prepare waveforms and output them
+    awg_wfm.prepare_initial()
+    awg_wfm.prepare_final()
+    # Set output channel to true
+    awg_wfm.get_output_channel(0).Output = True
+    awg_wfm.get_output_channel(1).Output = True
+    #
+    lab.PROC('fpga_dsp').reset_pipeline()
+    lab.PROC('fpga_dsp').add_stage(FPGA_DDC([[330e6],[]]))
+    lab.PROC('fpga_dsp').add_stage(FPGA_Decimation('sample', 10))
+    lab.PROC('fpga_dsp').add_stage(FPGA_FFT())
+    acq_module.set_data_processor(lab.PROC('fpga_dsp'))
+    acq_module.NumSamples = 10080
+    leData = acq_module.get_data()['data']
+    #
+    fig, ax = plt.subplots(nrows=2)
+    for r in range(acq_module.NumRepetitions) :
+        freqs = np.linspace(0, acq_module.SampleRate/10, 1024,endpoint=False)[:int(acq_module.NumSamples/10)]
+        ax[0].plot(freqs, np.abs(leData['data']['fft_real'][r] + 1j*leData['data']['fft_imag'][r]))
+        ax[1].plot(leData['data']['debug_time_I'][r])
+        ax[1].plot(leData['data']['debug_time_Q'][r])
+    #
+    lab.WFMT("OscMod").IQFrequency = 100e6
+
+
+def test_averaging(lab, num_points_per_corner=3, num_corners=12):
+    awg_wfm = lab.HAL('Waveform')
+    acq_module = lab.HAL('TaborACQ')
+
+    acq_module.NumSamples = 5040
+    acq_module.NumRepetitions = int(num_corners*num_points_per_corner)
+    fig, ax = plt.subplots(1)
+    for s in range(4):
+        pt_centre = 0.1*np.array([np.cos(2*np.pi/4*s), np.sin(2*np.pi/4*s)])
+
+        add_segments_circle(awg_wfm, pt_centre, num_corners, poly_size)
+        # Prepare waveforms and output them
+        awg_wfm.prepare_initial()
+        awg_wfm.prepare_final()
+        # Set output channel to true
+        awg_wfm.get_output_channel(0).Output = True
+        awg_wfm.get_output_channel(1).Output = True
+
+        lab.PROC('fpga_dsp').reset_pipeline()
+        lab.PROC('fpga_dsp').add_stage(FPGA_DDCFIR([[{'fLO':100e6, 'fc':10e6, 'Taps':40}, {'fLO':100e6, 'fc':10e6, 'Taps':40}]]))
+        lab.PROC('fpga_dsp').add_stage(FPGA_Decimation('sample', 10))
+        lab.PROC('fpga_dsp').add_stage(FPGA_Integrate('sample'))
+
+        acq_module.set_data_processor(lab.PROC('fpga_dsp'))
+        leData = acq_module.get_data()['data']
+
+        state_mkrs = ["o", ",", "d", "^", "s", "p", "h", "P"]
+        headers = acq_module._instr_acq.get_header_data(1)
+        headers = [state_mkrs[x['state1']] for x in headers]
+
+        lab.PROC('fpga_dsp').reset_pipeline()
+        lab.PROC('fpga_dsp').add_stage(FPGA_DDCFIR([[{'fLO':100e6, 'fc':10e6, 'Taps':40}, {'fLO':100e6, 'fc':10e6, 'Taps':40}]]))
+        lab.PROC('fpga_dsp').add_stage(FPGA_Decimation('sample', 10))
+        lab.PROC('fpga_dsp').add_stage(FPGA_Integrate('sample'))
+        lab.PROC('fpga_dsp').add_stage(FPGA_Integrate('repetition'))
+
+        acq_module.set_data_processor(lab.PROC('fpga_dsp'))
+
+        # acq_module._instr_acq._parent._send_cmd(':DSP:DEC:IQP:SEL DSP1')
+        # acq_module._instr_acq._parent._send_cmd(':DSP:DEC:IQP:OUTP SVM')
+        # acq_module._instr_acq._parent._send_cmd(':DSP:DEC:IQP:LINE 1,1,0')
+        # acq_module._instr_acq._parent._send_cmd(':DSP:DEC:IQP:LINE 2,-1,0')
+        # acq_module._instr_acq._parent._send_cmd(':DSP:DEC:IQP:LINE 3,0,0')
+
+        leData2 = acq_module.get_data()['data']
+
+        plt.gca().set_prop_cycle(None)
+        times = np.arange(acq_module.NumSamples)/acq_module.SampleRate * 1e9
+        for c in range(num_corners):
+            ax.scatter(leData['data']['CH1_0_I'][c::num_corners], leData['data']['CH1_0_Q'][c::num_corners], marker=str(s+1))
+        # for m in range(len(headers)):
+        #     ax.scatter([leData['data']['CH1_0_I'][m]], [leData['data']['CH1_0_Q'][m]], c='black', alpha=0.1, marker=headers[m])
+        ax.plot(leData2['data']['CH1_0_I']/acq_module.NumRepetitions, leData2['data']['CH1_0_Q']/acq_module.NumRepetitions, 'ko')
+        ax.plot(leData2['data']['CH1_0_I']/acq_module.NumRepetitions, leData2['data']['CH1_0_Q']/acq_module.NumRepetitions, marker=str(s+1))
+        x_vals = ax.get_xlim()
+        ax.plot(x_vals, np.array(x_vals), 'k-')
+        ax.plot(x_vals, -np.array(x_vals), 'k-')
+    ax.set_aspect('equal', 'box')
+
+
+def test_SVM(lab):
+    awg_wfm = lab.HAL('Waveform')
+    acq_module = lab.HAL('TaborACQ')
+
+    num_pts_axis = 10
+    acq_module.NumSamples = 5040
+    acq_module.NumRepetitions = int(num_pts_axis * num_pts_axis * 3)
+    fig, ax = plt.subplots(1)
+    #
+    awg_wfm.set_valid_total_time(499e-6)
+    awg_wfm.clear_segments()
+    awg_wfm.add_waveform_segment(WFS_Constant(f"init", lab.WFMT("OscMod").apply(phase=0), 96e-9, 0.25))
+    read_segs = []
+    #Form a grid in the IQ plane...
+    for x in np.linspace(-0.1,0.1, num_pts_axis):
+        for y in np.linspace(-0.1,0.1, num_pts_axis):
+            ampl, phs = np.sqrt(x**2+y**2), np.arctan2(y, x)
+            awg_wfm.add_waveform_segment(WFS_Constant(f"pad{x}{y}", None, 512e-9, 0.0))
+            awg_wfm.add_waveform_segment(WFS_Constant(f"constel{x}{y}", lab.WFMT("OscMod").apply(phase=phs), 2048e-9, ampl))
+            read_segs += [f"constel{x}{y}"]
+    awg_wfm.add_waveform_segment(WFS_Constant(f"pad", None, -1, 0.0))
+    awg_wfm.get_output_channel(0).marker(2).set_markers_to_segments(read_segs)
+    awg_wfm.prepare_initial()
+    awg_wfm.prepare_final()
+    awg_wfm.get_output_channel(0).Output = True
+    awg_wfm.get_output_channel(1).Output = True
+    #
     lab.PROC('fpga_dsp').reset_pipeline()
     lab.PROC('fpga_dsp').add_stage(FPGA_DDCFIR([[{'fLO':100e6, 'fc':10e6, 'Taps':40}, {'fLO':100e6, 'fc':10e6, 'Taps':40}]]))
     lab.PROC('fpga_dsp').add_stage(FPGA_Decimation('sample', 10))
     lab.PROC('fpga_dsp').add_stage(FPGA_Integrate('sample'))
-    lab.PROC('fpga_dsp').add_stage(FPGA_Integrate('repetition'))
-
-    acq_module.set_data_processor(lab.PROC('fpga_dsp'))
-
+    #
     # acq_module._instr_acq._parent._send_cmd(':DSP:DEC:IQP:SEL DSP1')
     # acq_module._instr_acq._parent._send_cmd(':DSP:DEC:IQP:OUTP SVM')
     # acq_module._instr_acq._parent._send_cmd(':DSP:DEC:IQP:LINE 1,1,0')
     # acq_module._instr_acq._parent._send_cmd(':DSP:DEC:IQP:LINE 2,-1,0')
     # acq_module._instr_acq._parent._send_cmd(':DSP:DEC:IQP:LINE 3,0,0')
-
-    leData2 = acq_module.get_data()['data']
-
+    acq_module.set_decision_block([DEC_SVM([(-1,1,0), (1,1,0), (0,1,0)]), None])
+    #
+    acq_module.set_data_processor(lab.PROC('fpga_dsp'))
+    rawData = acq_module.get_data()
+    leData = rawData['data']
+    #
+    state_mkrs = ["o", ",", "d", "^", "s", "p", "h", "P"]
+    headers = rawData['decisions']
+    headers = [state_mkrs[x] for x in headers['data']['state1']]
+    #
     plt.gca().set_prop_cycle(None)
-    for sg in range(acq_module.NumSegments) :
-        times = np.arange(acq_module.NumSamples)/acq_module.SampleRate * 1e9
-        for c in range(num_corners):
-            ax.scatter(leData['data']['CH1_0_I'][sg][c::num_corners], leData['data']['CH1_0_Q'][sg][c::num_corners], marker=str(s+1))
-        for m in range(len(headers)):
-            ax.scatter([leData['data']['CH1_0_I'][sg][m]], [leData['data']['CH1_0_Q'][sg][m]], c='black', alpha=0.1, marker=headers[m])
-    ax.plot(leData2['data']['CH1_0_I']/acq_module.NumRepetitions, leData2['data']['CH1_0_Q']/acq_module.NumRepetitions, 'ko')
-    ax.plot(leData2['data']['CH1_0_I']/acq_module.NumRepetitions, leData2['data']['CH1_0_Q']/acq_module.NumRepetitions, marker=str(s+1))
-    x_vals = ax.get_xlim()
-    ax.plot(x_vals, np.array(x_vals), 'k-')
-    ax.plot(x_vals, -np.array(x_vals), 'k-')
-ax.set_aspect('equal', 'box')
-
-
-####################
-#SVM DECISION TESTS#
-####################
-num_pts_axis = 10
-acq_module.NumSamples = 5040
-acq_module.NumRepetitions = int(num_pts_axis * num_pts_axis * 3)
-fig, ax = plt.subplots(1)
-#
-awg_wfm.set_valid_total_time(499e-6)
-awg_wfm.clear_segments()
-awg_wfm.add_waveform_segment(WFS_Constant(f"init", lab.WFMT("OscMod").apply(phase=0), 96e-9, 0.25))
-read_segs = []
-#Form a grid in the IQ plane...
-for x in np.linspace(-0.1,0.1, num_pts_axis):
-    for y in np.linspace(-0.1,0.1, num_pts_axis):
-        ampl, phs = np.sqrt(x**2+y**2), np.arctan2(y, x)
-        awg_wfm.add_waveform_segment(WFS_Constant(f"pad{x}{y}", None, 512e-9, 0.0))
-        awg_wfm.add_waveform_segment(WFS_Constant(f"constel{x}{y}", lab.WFMT("OscMod").apply(phase=phs), 2048e-9, ampl))
-        read_segs += [f"constel{x}{y}"]
-awg_wfm.add_waveform_segment(WFS_Constant(f"pad", None, -1, 0.0))
-awg_wfm.get_output_channel(0).marker(2).set_markers_to_segments(read_segs)
-awg_wfm.prepare_initial()
-awg_wfm.prepare_final()
-awg_wfm.get_output_channel(0).Output = True
-awg_wfm.get_output_channel(1).Output = True
-#
-lab.PROC('fpga_dsp').reset_pipeline()
-lab.PROC('fpga_dsp').add_stage(FPGA_DDCFIR([[{'fLO':100e6, 'fc':10e6, 'Taps':40}, {'fLO':100e6, 'fc':10e6, 'Taps':40}]]))
-lab.PROC('fpga_dsp').add_stage(FPGA_Decimation('sample', 10))
-lab.PROC('fpga_dsp').add_stage(FPGA_Integrate('sample'))
-#
-# acq_module._instr_acq._parent._send_cmd(':DSP:DEC:IQP:SEL DSP1')
-# acq_module._instr_acq._parent._send_cmd(':DSP:DEC:IQP:OUTP SVM')
-# acq_module._instr_acq._parent._send_cmd(':DSP:DEC:IQP:LINE 1,1,0')
-# acq_module._instr_acq._parent._send_cmd(':DSP:DEC:IQP:LINE 2,-1,0')
-# acq_module._instr_acq._parent._send_cmd(':DSP:DEC:IQP:LINE 3,0,0')
-acq_module.set_decision_block([DEC_SVM([(-1,1,0), (1,1,0), (0,1,0)]), None])
-#
-acq_module.set_data_processor(lab.PROC('fpga_dsp'))
-rawData = acq_module.get_data()
-leData = rawData['data']
-#
-state_mkrs = ["o", ",", "d", "^", "s", "p", "h", "P"]
-headers = rawData['decisions']
-headers = [state_mkrs[x] for x in headers['data']['state1']]
-#
-plt.gca().set_prop_cycle(None)
-for sg in range(acq_module.NumSegments) :
     times = np.arange(acq_module.NumSamples)/acq_module.SampleRate * 1e9
     # for c in range(num_corners):
     #     ax.scatter(leData['data']['CH1_0_I'][sg][c::num_corners], leData['data']['CH1_0_Q'][sg][c::num_corners], marker=str(s+1))
     for m in range(len(headers)):
-        ax.scatter([leData['data']['CH1_0_I'][sg][m]], [leData['data']['CH1_0_Q'][sg][m]], c='black', alpha=0.1, marker=headers[m])
-x_vals = ax.get_xlim()
-ax.plot(x_vals, np.array(x_vals), 'k-')
-ax.plot(x_vals, -np.array(x_vals), 'k-')
-ax.plot(x_vals, 0*np.array(x_vals), 'k-')
-ax.set_aspect('equal', 'box')
+        ax.scatter([leData['data']['CH1_0_I'][m]], [leData['data']['CH1_0_Q'][m]], c='black', alpha=0.1, marker=headers[m])
+    x_vals = ax.get_xlim()
+    ax.plot(x_vals, np.array(x_vals), 'k-')
+    ax.plot(x_vals, -np.array(x_vals), 'k-')
+    ax.plot(x_vals, 0*np.array(x_vals), 'k-')
+    ax.set_aspect('equal', 'box')
 
-ExperimentConfiguration("test", lab, 1e-3, ['Waveform'], 'TaborACQ')
-exp = Experiment('test', lab.CONFIG('test'))
-leData = lab.run_single(exp)
+    ExperimentConfiguration("test", lab, 1e-3, ['Waveform'], 'TaborACQ')
+    exp = Experiment('test', lab.CONFIG('test'))
+    leData = lab.run_single(exp)
+
+
+test_basic_time_traces(lab)
+# test_FFT(lab)
+test_averaging(lab, 1000)
 
 plt.show()
 awg_wfm.get_output_channel(0).Output = False
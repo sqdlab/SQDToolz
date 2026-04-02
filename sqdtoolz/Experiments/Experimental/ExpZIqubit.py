63b7a8c6581344160ae93f1baaf4f98db8bf73f7
6biscuits <zachdegnan96@gmail.com>
2026-04-02 16:21:36 +1000

    Aded pulse-sheet and execution time outputs to ExpZIqubit

--- sqdtoolz/Experiments/Experimental/ExpZIqubit.py
+++ sqdtoolz/Experiments/Experimental/ExpZIqubit.py
@@ -3,6 +3,10 @@ from sqdtoolz.HAL.ZI.ZIACQ import ZIACQ
 from laboneq.workflow.logbook import LoggingStore, DEFAULT_LOGGING_STORE, FolderStore
 import logging
 from laboneq.laboneq_logging import set_log_dir
+#
+import inspect
+from laboneq.workflow.tasks import compile_experiment
+from laboneq.pulse_sheet_viewer import pulse_sheet_viewer
 
 class ExpZIqubit(Experiment):
     def __init__(self, name, expt_config, workflow_module, hal_QPU, qubit_ids, **kwargs):
@@ -16,17 +20,19 @@ class ExpZIqubit(Experiment):
         self._transition = kwargs.pop('transition', 'ge')
         self._plot_ZI = kwargs.pop('ZI_plot', False)
         self._args = kwargs
+        self._show_pulse_sheet = kwargs.pop('show_pulse_sheet', False)
     
     def _run(self, file_path, sweep_vars=[], **kwargs):
         self._expt_config.init_instruments()
 
         leACQ = self._expt_config._hal_ACQ
-        assert isinstance(leACQ, ZIACQ), "The ExperimentConfiguration must has a ZIACQ object as its acquisition HAL."
+        assert isinstance(leACQ, ZIACQ), "The ExperimentConfiguration must have a ZIACQ object as its acquisition HAL."
         options = self._workflow_module.experiment_workflow.options()
         leACQopts = leACQ.get_ZI_parameters()
         for x in leACQopts:
             getattr(options, x)(leACQopts[x])
-        getattr(options, 'update')(self._update_params)
+        if hasattr(options, 'update'):
+            getattr(options, 'update')(self._update_params)
         #TODO: Put this into an options parser/dictionary...
         if hasattr(options, 'use_cal_traces'):
             getattr(options, 'use_cal_traces')(self._normalise_data)
@@ -54,22 +60,35 @@ class ExpZIqubit(Experiment):
         for handler in leLogger.handlers:
             if isinstance(handler, logging.StreamHandler):
                 leLogger.removeHandler(handler)
-        try:#TODO: Don't do this...
-            exp_workflow = self._workflow_module.experiment_workflow(
-                session=leSession,
-                qpu=leQPU,
-                qubits=leQubits,
-                options=options,
-                **self._args
-            )
-        except:
-            exp_workflow = self._workflow_module.experiment_workflow(
-                session=leSession,
-                qpu=leQPU,
-                qubit=leQubits[0],
-                options=options,
-                **self._args
-            )
+        
+        sig = inspect.signature(self._workflow_module.create_experiment)
+        zi_exp_params = list(sig.parameters.keys())
+        if 'qubit' in zi_exp_params:
+            qubit_kwarg = {'qubit': leQubits[0]}
+        else:
+            qubit_kwarg = {'qubits': leQubits}
+        
+        print_pulse_sheet = kwargs.pop('print_pulse_sheet',True)
+        print_est_time =  kwargs.pop('print_estimated_execution_time',True)
+        if print_pulse_sheet or print_est_time:
+            temp_exp = self._workflow_module.create_experiment(
+                        leQPU,
+                        **qubit_kwarg,
+                        **self._args)
+            compiled_exp = compile_experiment(leSession, temp_exp)
+        if print_pulse_sheet:
+            pulse_sheet_viewer.show_pulse_sheet(file_path+'timing_diagram', compiled_exp)
+        if print_est_time:      
+            print(f"Expected Runtime: {compiled_exp.estimated_runtime:.3f}s")
+
+        exp_workflow = self._workflow_module.experiment_workflow(
+            session=leSession,
+            qpu=leQPU,
+            **qubit_kwarg,
+            options=options,
+            **self._args
+        )
+
         self._expt_config._hal_ACQ._cur_workflow = exp_workflow
         
         kwargs['skip_init_instruments'] = True

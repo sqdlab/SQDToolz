7a023972072acf6fa0f227024f99d9b10ee3699e
WilliamCam <78771433+WilliamCam@users.noreply.github.com>
2026-07-30 16:04:49 +1000

    FluxScope changes
    squash

--- sqdtoolz/Experiments/Experimental/ExpZIFluxscope.py
+++ sqdtoolz/Experiments/Experimental/ExpZIFluxscope.py
@@ -8,37 +8,47 @@ from sqdtoolz.Utilities.FileIO import FileIODirectory
 from pathlib import Path
 import sqdtoolz as stz
 
-class ExpZIFluxscope(ExpZIqubit):
+class ExpZIFluxscope:
     def __init__(self, name, expt_config, hal_QPU, qubit_ids, **kwargs):
         self._expt_config= expt_config
         self._hal_QPU = hal_QPU
         self._isY = 0 #Will be passed to cryo_scope to determine x90 or y90
         self._qubit_ids = qubit_ids
+        self._kwargs = kwargs
         assert len(self._qubit_ids)==2, "Also must define a second qubit as Will attempt to optimise a pulse specific to each coupler element"
         
 
         self._amplitudes = kwargs.get('amplitudes', np.linspace(0.1,1.0,10))
         self._lengths = kwargs.get('lengths', np.linspace(10e-9, 100e-9, 10))
-        self._return_data = []
-        super().__init__(name, expt_config, cryo_scope, hal_QPU, qubit_ids, 
+        self.data = {}
+
+    def run(self, lab):
+        qubit = self._qubit_ids[0]
+        lab.group_open(self._name)
+        exp_X = ExpZIqubit(f'cryoscope_{qubit}_X', self._expt_config, cryo_scope, self._hal_QPU, self._qubit_ids, 
                          lengths = self._lengths, 
                          amplitudes=self._amplitudes,
-                         y90 = bool(self._isY), #pass bool to cryoscope
-                         **kwargs
+                         y90 = False
+                         **self._kwargs
                          )
-    
-    def _run(self, file_path, sweep_vars=[], **kwargs):
-        assert len(sweep_vars) == 0, "Supply the pulse amplitudes/lengths when defining the Experiment object."
-        var_xy = VariablePropertyTransient('Axis', self, '_isY')
-        super()._run(file_path, sweep_vars=[(var_xy, np.array([1, 0]))], **kwargs)
-    
-    def _post_process(self, data):
-        qubit = self._qubit_ids[0]
-        data = self.retrieve_last_dataset(qubit)
-        #amplitudes, lengths
-        XY = data.param_names
-        arr = data.get_numpy_array()
-        self._return_data
+        lab.run_single(exp_X)
+
+        dataX = self.retrieve_last_dataset(qubit)
+        self.data['X'] = dataX.get_numpy_array()
+
+        exp_Y = ExpZIqubit(f'cryoscope_{qubit}_Y', self._expt_config, cryo_scope, self._hal_QPU, self._qubit_ids, 
+                        lengths = self._lengths, 
+                        amplitudes=self._amplitudes,
+                        y90 = True
+                        **self._kwargs
+                        )
+        lab.run_single(exp_Y)
+
+        dataY = self.retrieve_last_dataset(qubit)
+        self.data['Y'] = dataY.get_numpy_array()
+
+        lab.group_close()
+
         
 
 
